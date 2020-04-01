74422ddc5c46672a5cfb7ac1107e37add77ae98d
Dylan Bedetti <dylanbedetti@gmail.com>
2020-04-01 22:09:36 +0800

    dope af

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # to DO
-cant remove first item
-when clicking done check if any empty/more than 2 items
-create better voting method
+
+show when they are equal?
--- js/main.js
+++ js/main.js
@@ -23,66 +23,81 @@ $(document).ready(function() {
 
     // console.log(itemObject);
     total_questions = sumAll(itemLength);
-    progress_div = 100/total_questions;
+    progress_div = 100 / total_questions;
     $("#progressbar").css("width", "1%");
 
     $("#priorityElement1").text(Object.keys(itemObject)[i]);
     $("#priorityElement2").text(Object.keys(itemObject)[j]);
   }
 
-  $("#listComparison").on(
-    "click",
-    "#priorityElement1, #priorityElement2",
-    function() {
-      // increment key of object
-      itemObject[$(this).text()] = itemObject[$(this).text()] + 1;
+  $(".multi-button-prefernce").on("click", e => {
+    console.log($(e.target).attr("id"));
+    $btn_id = $(e.target).attr("id");
 
-      $("#progressbar").css("width", `${current_percent+progress_div}%`)
-      current_percent += progress_div;
+    switch ($btn_id) {
+      case "btn_1":
+        itemObject[$("#priorityElement1").text()] =
+          itemObject[$("#priorityElement1").text()] + 4;
+        break;
+      case "btn_2":
+        itemObject[$("#priorityElement1").text()] =
+          itemObject[$("#priorityElement1").text()] + 1;
+        break;
+      case "btn_3":
+        itemObject[$("#priorityElement2").text()] =
+          itemObject[$("#priorityElement2").text()] + 1;
+        break;
+      case "btn_4":
+        itemObject[$("#priorityElement2").text()] =
+          itemObject[$("#priorityElement2").text()] + 4;
+        break;
+    }
 
+    console.log(itemObject);
 
-      if (j < itemLength - 1) {
-        j++;
-        console.log(i, j);
-        $("#priorityElement2").text(Object.keys(itemObject)[j]);
-      } else if (i < itemLength - 2) {
-        i++;
-        j = i + 1;
-        console.log(i, j);
-        $("#priorityElement1").text(Object.keys(itemObject)[i]);
-        $("#priorityElement2").text(Object.keys(itemObject)[j]);
-      } else {
-        console.log("done!");
+    // update progress bar
+    $("#progressbar").css("width", `${current_percent + progress_div}%`);
+    current_percent += progress_div;
 
-        $("#listComparison").hide();
+    // looping through list
+    if (j < itemLength - 1) {
+      // first limit
+      j++;
+      $("#priorityElement2").text(Object.keys(itemObject)[j]);
+    } else if (i < itemLength - 2) {
+      // second element
+      i++;
+      j = i + 1;
+      $("#priorityElement1").text(Object.keys(itemObject)[i]);
+      $("#priorityElement2").text(Object.keys(itemObject)[j]);
+    } else {
+      // done
+      $("#listComparison").hide();
 
-        var sortedList = [];
-        for (var el in itemObject) {
-          sortedList.push([el, itemObject[el]]);
-        }
-
-        sortedList.sort(function(a, b) {
-          return a[1] - b[1];
-        });
-
-        console.log(sortedList);
-
-        $("#orderedListItems").empty();
-
-        for (let i = sortedList.length - 1; i >= 0; i--) {
-          var newContent =
-            `<li class="list-group-item final-list">` +
-            sortedList[i][0] +
-            `</li>`;
-          $("#orderedListItems").append(newContent);
-        }
-
-        $("#orderedList").show();
+      var sortedList = [];
+      for (var el in itemObject) {
+        sortedList.push([el, itemObject[el]]);
       }
 
-      console.log(itemObject);
+      sortedList.sort(function(a, b) {
+        return a[1] - b[1];
+      });
+
+      console.log(sortedList);
+
+      $("#orderedListItems").empty();
+
+      for (let i = sortedList.length - 1; i >= 0; i--) {
+        var newContent =
+          `<li class="list-group-item final-list">` +
+          sortedList[i][0] +
+          `</li>`;
+        $("#orderedListItems").append(newContent);
+      }
+
+      $("#orderedList").show();
     }
-  );
+  });
 
   // Add new input element when clicking "new Item"
   $("#newItemButton").on("click", () => {
@@ -118,6 +133,17 @@ $(document).ready(function() {
 
   // Removing input element when remove button pressud
   $("#prioritiesList").on("click", ".remove_btn", e => {
+    if ($("#prioritiesList input").length <= 1) {
+      $("#prioritiesList input")
+        .parent()
+        .fadeOut(100)
+        .fadeIn(100)
+        .fadeOut(100)
+        .fadeIn(100);
+      $("#prioritiesList div span").css("background-color", "#e9ecef");
+      return false;
+    }
+
     var $input_container = $(e.target)
       .parent()
       .parent();
@@ -126,14 +152,32 @@ $(document).ready(function() {
     });
   });
 
+  // alert exist button
+  $("#alert_div").on("click", "button", e => {
+    $(e.target)
+      .parent()
+      .parent()
+      .fadeOut();
+  });
   // Moving webapp to stage 2 - removing input fields, buttons and storing items in objects
   $("#doneButton").on("click", () => {
     console.log("done");
 
+    if ($("input:text").length < 2) {
+      console.log("fuck");
+      $("#alert_div").fadeIn();
+      $("#alert_div")
+        .delay(5000)
+        .fadeOut();
+      return false;
+    }
+
     // initialising zeros for array
     itemObject = {};
     $("input:text").each(function() {
-      itemObject[$(this).val()] = 0;
+      if ($(this).val() != ""){
+        itemObject[$(this).val()] = 0;
+      }
     });
 
     itemLength = Object.keys(itemObject).length;
@@ -164,5 +208,5 @@ $(document).ready(function() {
 
   // enabling information tooltip
   $('[data-toggle="tooltip"]').tooltip();
-  // $('.progress .progress-bar').progressbar();  
+  // $('.progress .progress-bar').progressbar();
 });
