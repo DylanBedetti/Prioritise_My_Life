f00a9294395e625f33688beddf1e0f2381c54d11
Dylan Bedetti <dylanbedetti@gmail.com>
2020-04-01 20:50:25 +0800

    dopeaf

---
+++ README.md
@@ -0,0 +1,4 @@
+# to DO
+cant remove first item
+when clicking done check if any empty/more than 2 items
+create better voting method
--- js/main.js
+++ js/main.js
@@ -3,131 +3,166 @@ $(document).ready(function() {
   var i = 0;
   var j = 1;
   var itemLength;
+  var total_questions;
+  var progress_div;
+  var current_percent;
   //   console.log("ready!");
+  function sumAll(n) {
+    total = 0;
+    for (i = n - 1; i > 0; i--) {
+      total = total + i;
+    }
+    return total;
+  }
 
   // function to populate selections
   function isVisible() {
-    console.log("stage 2 visible");
-    console.log(itemObject);
-
     i = 0;
     j = 1;
+    current_percent = 0;
+
+    // console.log(itemObject);
+    total_questions = sumAll(itemLength);
+    progress_div = 100/total_questions;
+    $("#progressbar").css("width", "1%");
 
     $("#priorityElement1").text(Object.keys(itemObject)[i]);
     $("#priorityElement2").text(Object.keys(itemObject)[j]);
   }
 
-  $(document).on("click", "#priorityElement1, #priorityElement2", function() {
-    // increment key of object
-    itemObject[$(this).text()] = itemObject[$(this).text()] + 1;
+  $("#listComparison").on(
+    "click",
+    "#priorityElement1, #priorityElement2",
+    function() {
+      // increment key of object
+      itemObject[$(this).text()] = itemObject[$(this).text()] + 1;
 
-    if (j < itemLength - 1) {
-      j++;
-      console.log(i, j);
-      $("#priorityElement2").text(Object.keys(itemObject)[j]);
-    } else if (i < itemLength - 2) {
-      i++;
-      j = i + 1;
-      console.log(i, j);
-      $("#priorityElement1").text(Object.keys(itemObject)[i]);
-      $("#priorityElement2").text(Object.keys(itemObject)[j]);
-    } else {
-      console.log("done!");
+      $("#progressbar").css("width", `${current_percent+progress_div}%`)
+      current_percent += progress_div;
 
-      $("#listComparison").toggle();
 
-      var sortedList = [];
-      for (var el in itemObject) {
-        sortedList.push([el, itemObject[el]]);
+      if (j < itemLength - 1) {
+        j++;
+        console.log(i, j);
+        $("#priorityElement2").text(Object.keys(itemObject)[j]);
+      } else if (i < itemLength - 2) {
+        i++;
+        j = i + 1;
+        console.log(i, j);
+        $("#priorityElement1").text(Object.keys(itemObject)[i]);
+        $("#priorityElement2").text(Object.keys(itemObject)[j]);
+      } else {
+        console.log("done!");
+
+        $("#listComparison").hide();
+
+        var sortedList = [];
+        for (var el in itemObject) {
+          sortedList.push([el, itemObject[el]]);
+        }
+
+        sortedList.sort(function(a, b) {
+          return a[1] - b[1];
+        });
+
+        console.log(sortedList);
+
+        $("#orderedListItems").empty();
+
+        for (let i = sortedList.length - 1; i >= 0; i--) {
+          var newContent =
+            `<li class="list-group-item final-list">` +
+            sortedList[i][0] +
+            `</li>`;
+          $("#orderedListItems").append(newContent);
+        }
+
+        $("#orderedList").show();
       }
 
-      sortedList.sort(function(a, b) {
-        return a[1] - b[1];
-      });
-
-      console.log(sortedList);
-
-      $("#orderedListItems").empty();
-
-      for (let i = sortedList.length - 1; i >= 0; i--) {
-        var newContent =
-          `<li class="list-group-item final-list">` +
-          Object.keys(itemObject)[i] +
-          `</li>`;
-        $("#orderedListItems").append(newContent);
-      }
-
-      $("#orderedList").removeClass("d-none");
+      console.log(itemObject);
     }
-
-    console.log(itemObject);
-  });
+  );
 
   // Add new input element when clicking "new Item"
-  $("#newItemButton *").click(() => {
-    var newContent = `
-		<div class="input-group mb-3">
-			<input type="text" class="form-control" placeholder="Item" />
-			<div class="input-group-append">
-				<span class="input-group-text" style="cursor:pointer;">remove</span>
-			</div>
-		</div>`;
-    $("#prioritiesList").append(newContent);
+  $("#newItemButton").on("click", () => {
+    var $newContent = $("#prioritiesList")
+      .children(":last")
+      .clone();
+    $newContent.find("input").val("");
+    $("#prioritiesList").append($newContent);
+
+    $("#prioritiesList")
+      .children(":last")
+      .find("input")
+      .select();
     console.log("button press");
   });
 
+  // remove button - used document because global?
+  $("#prioritiesList").on("mouseover", ".remove_btn", e => {
+    $(e.target).css("background-color", "#db9ca0");
+  });
+
+  $("#prioritiesList").on("mouseout", ".remove_btn", e => {
+    $(e.target).css("background-color", "#e9ecef");
+  });
+
   // Calling new input item event listener when enter button is used
-  $(document).on("keypress", ".form-control", function(e) {
+  $("#prioritiesList").on("keypress", ".form-control", function(e) {
     if (e.which == 13) {
-      $("#newItemButton *").click();
+      $("#newItemButton").click();
       $("input:text:visible:last").focus();
     }
   });
 
   // Removing input element when remove button pressud
-  $(document).on("click", ".input-group-append", event => {
-    console.log("remove pressed", event.target);
-    $(event.target)
+  $("#prioritiesList").on("click", ".remove_btn", e => {
+    var $input_container = $(e.target)
       .parent()
-      .parent()
-      .remove();
+      .parent();
+    $input_container.slideUp("slow", () => {
+      $input_container.remove();
+    });
   });
 
   // Moving webapp to stage 2 - removing input fields, buttons and storing items in objects
-  $(document).on("click", "#doneButton *", () => {
+  $("#doneButton").on("click", () => {
     console.log("done");
-    $(
-      "#prioritiesList, #newItemButton, #doneButton, #instructionParagraph"
-    ).toggle("slide", { direction: "left" }, 500);
 
-    itemObject = [];
+    // initialising zeros for array
+    itemObject = {};
     $("input:text").each(function() {
       itemObject[$(this).val()] = 0;
-      console.log($(this).val());
     });
+
+    itemLength = Object.keys(itemObject).length;
+
+    isVisible();
+
     console.log(itemObject);
 
-    $("#previousButton, #listComparison")
+    $("#page_1").toggle("slide", { direction: "left" }, 500);
+
+    $("#page_2")
       .delay(500)
       .fadeIn(500);
 
-    itemLength = Object.keys(itemObject).length;
-    isVisible();
+    $("#listComparison").show();
   });
 
   // enabling previous button
-  $(document).on("click", "#previousButtonClick", () => {
-    // $("#doneButton *").click();
-    $("#previousButton, #listComparison").fadeOut(500);
+  $("#previousButton").on("click", () => {
+    $("#page_2").fadeOut(500);
 
-    $("#prioritiesList, #newItemButton, #doneButton, #instructionParagraph")
+    $("#page_1")
       .delay(600)
       .toggle("slide", { direction: "left" }, 500);
 
-    console.log("previous");
-    $("#orderedList").addClass("d-none");
+    $("#orderedList").hide();
   });
 
   // enabling information tooltip
   $('[data-toggle="tooltip"]').tooltip();
+  // $('.progress .progress-bar').progressbar();  
 });
